85223ea3b6805de0e1628370736232e169f478dc
yina00 <20221181@gmail.com>
2025-05-15 02:08:55 +0900

    초기 설정 커밋

---
+++ src/main/java/com/example/sungshinfarm/SungshinfarmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sungshinfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungshinfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungshinfarmApplication.class, args);
+	}
+
+}
