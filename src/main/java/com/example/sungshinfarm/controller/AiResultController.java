e0116d37510caaa33b12955b9d742addfbd375aa
yina00 <20221181@gmail.com>
2025-05-16 19:05:12 +0900

    로그 추가

--- src/main/java/com/example/sungshinfarm/controller/AiResultController.java
+++ src/main/java/com/example/sungshinfarm/controller/AiResultController.java
@@ -10,6 +10,7 @@ public class AiResultController {
     @PostMapping("/predict-result")
     public AiResultDto receivePrediction(@RequestBody AiResultDto resultDto) {
         // 여기서는 단순히 받은 데이터를 그대로 반환만 합니다.
+        System.out.println("AI가 보낸 데이터: " + resultDto);
         return resultDto;
     }
 }
